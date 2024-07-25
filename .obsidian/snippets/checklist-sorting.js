971da61d2b3328c294e8bb0a988c255aac7b7128
Shivansh Shukla <cashivanshshukla.coding@gmail.com>
2024-07-25 09:45:21 +0530

    added and zip of .obsidian

--- .obsidian/snippets/checklist-sorting.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let list = document.querySelector(".markdown-preview-view ul.task-list");
-  let items = Array.from(list.children);
-  items.forEach((item) => {
-    if (item.querySelector("input").checked) {
-      list.appendChild(item);
-    }
-  });
-});
